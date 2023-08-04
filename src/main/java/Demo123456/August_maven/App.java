0ad1d8cbf53d9347ecade1f43374d4047a77714f
HP <HP@DESKTOP-E9J2K2J>
2023-08-04 11:35:46 +0530

    first COMIT

---
+++ src/main/java/Demo123456/August_maven/App.java
@@ -0,0 +1,13 @@
+package Demo123456.August_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
